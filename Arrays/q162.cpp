0fa1376343961668fa09b08fc234172875020b53
Pranav Khurana <pranavkhurana69@gmail.com>
2026-02-25 13:04:03 +0530

    Q162

--- Arrays/q162.cpp
+++ Arrays/q162.cpp
@@ -2,46 +2,41 @@
 
 #include<iostream>
 #include<vector>
+#include<limits.h>
 
 using namespace std;
 
 int findPeakElement(vector<int>& nums);
 
 int main(void){
-    vector<int> retVal = {1, 2, 3, 1};
+    vector<int> retVal = {1,2,1,3,5,6,4};
 
-    cout << "The peak element in the given array is: " << findPeakElement(retVal);
+    cout << "The index of the largest number is: " << findPeakElement(retVal);
 
     return EXIT_SUCCESS;
 }
 
 int findPeakElement(vector<int>& nums){
     int n = nums.size();
-
     int st = 0;
     int end = n - 1;
 
-    while(st <= end){
-        int mid = st + (end-st)/2;
+    nums[-1] = INT_MIN;
+    nums[n] = INT_MIN;
 
-        if(mid > 0 && mid < n - 1){
-            if(nums[mid] > nums[mid+1]){
-                if(nums[mid] > nums[mid -1]){
-                    return mid;
-                }else{
-                    end = mid - 1;
-                }
-            }else{
-                st = mid + 1;
-            }
-        }else if(mid == 0){
-            if(nums[mid] > nums[mid + 1]){
-                return mid;
-            }
-        }else{
-            if(nums[mid] > nums[mid - 1]){
-                return mid;
-            }
+    while( st <= end){
+        int mid = st + (end - st)/2;
+
+        if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]){
+            return mid;
+        }
+
+        if(nums[mid] < nums[mid + 1]){
+            st = mid + 1;
+        }
+
+        if(nums[mid] < nums[mid - 1]){
+            end = mid - 1;
         }
     }
 
